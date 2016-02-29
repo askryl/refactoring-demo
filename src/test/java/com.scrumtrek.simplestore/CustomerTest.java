2c56f3742c7eaba0f5e7697008525bafd24b4942
Anna <annaalkh@yandex.ru>
2016-02-29 15:17:48 +0300

    customer test 2

--- src/test/java/com.scrumtrek.simplestore/CustomerTest.java
+++ src/test/java/com.scrumtrek.simplestore/CustomerTest.java
@@ -2,6 +2,8 @@ package com.scrumtrek.simplestore;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertEquals;
 import static org.fest.assertions.Assertions.*;
 
@@ -23,6 +25,24 @@ public class CustomerTest {
 
         //region Then
         assertThat(result).contains("Amount owed is 0");
+        //endregion
+    }
+
+    @Test
+    public void shouldRegularMovieAmountAndFreqPointsWhenGetStatementDays1() {
+
+        //region Given
+        Customer sut = new Customer("Test customer");
+        sut.addRental(new Rental(new Movie("MovieTitle", PriceCodes.Regular), 1));
+        //endregion
+
+        //region When
+        String result = sut.Statement();
+        //endregion
+
+        //region Then
+        assertThat(result).contains("Amount owed is 2");
+        assertThat(result).contains("You earned 1 frequent renter points.");
         //endregion
     }
 }
